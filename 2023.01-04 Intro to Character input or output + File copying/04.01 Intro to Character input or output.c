87a37bb8b5373154e04d2c0d68dd1211af9ecb6a
Paxyam <paxtonaltman@gmail.com>
2023-01-11 09:19:53 -0800

    2023.01.11 9a

---
+++ 2023.01-04 Intro to Character input or output + File copying/04.01 Intro to Character input or output.c
@@ -0,0 +1,29 @@
+//page 29
+//text stream - sequence of newline chracacters divided into lines,
+//each line consists of zero or more characters followed by newline character,
+//library job
+//we (the programmer) dont need to worry about how lines look outside the program
+
+/*
+main()
+{
+    c=getchar()
+}
+*/
+
+//getchar reads the next input character from a text stream 
+//and returns that as its value
+
+//the variable c contains the next character of output
+
+/*
+main()
+{
+    putchar(c)
+}
+*/
+
+//prints the contents of the integer variable 'c' as a character, 
+//usually on screen
+//'putchar' and 'printf' may be interleaved, the output will
+//appear in the order in which the calls are made
